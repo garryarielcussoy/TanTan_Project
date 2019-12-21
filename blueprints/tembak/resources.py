9e8da229edf3021bd759a383b46afa560baf2961
Garry Ariel <garry@alterra.id>
2019-12-21 16:07:27 +0700

    Commit

--- blueprints/tembak/resources.py
+++ blueprints/tembak/resources.py
@@ -26,46 +26,57 @@ class Conversation(Resource):
         args = parser.parse_args()
 
         # Checking horroscope
-        if re.search(r"[Bb][Ii]*[Nn]+[Gg]+[Uu]*[Nn]+[Gg]+", args['text']):
-            day = int(args['date_birth'][0:2])
-            month = int(args['date_birth'][3:5])
-            
-            if month == 12:
-                astro_sign = 'Sagittarius' if (day < 22) else 'Capricorn'
-            elif month == 1:
-                astro_sign = 'Capricorn' if (day < 20) else 'Aquarius'
-            elif month == 2:
-                astro_sign = 'Aquarius' if (day < 19) else 'Pisces'
-            elif month == 3:
-                astro_sign = 'Pisces' if (day < 21) else 'Aries'
-            elif month == 4:
-                astro_sign = 'Aries' if (day < 20) else 'Taurus'
-            elif month == 5:
-                astro_sign = 'Taurus' if (day < 21) else 'Gemini'
-            elif month == 6:
-                astro_sign = 'Gemini' if (day < 21) else 'Cancer'
-            elif month == 7:
-                astro_sign = 'Cancer' if (day < 23) else 'Leo'
-            elif month == 8:
-                astro_sign = 'Leo' if (day < 23) else 'Virgo'
-            elif month == 9:
-                astro_sign = 'Virgo' if (day < 23) else 'Libra'
-            elif month == 10:
-                astro_sign = 'Libra' if (day < 23) else 'Scorpio'
-            elif month == 11:
-                astro_sign = 'Scorpio' if (day < 22) else 'Sagittarius'
+        day = int(args['date_birth'][0:2])
+        month = int(args['date_birth'][3:5])
+        
+        if month == 12:
+            astro_sign = 'Sagittarius' if (day < 22) else 'Capricorn'
+        elif month == 1:
+            astro_sign = 'Capricorn' if (day < 20) else 'Aquarius'
+        elif month == 2:
+            astro_sign = 'Aquarius' if (day < 19) else 'Pisces'
+        elif month == 3:
+            astro_sign = 'Pisces' if (day < 21) else 'Aries'
+        elif month == 4:
+            astro_sign = 'Aries' if (day < 20) else 'Taurus'
+        elif month == 5:
+            astro_sign = 'Taurus' if (day < 21) else 'Gemini'
+        elif month == 6:
+            astro_sign = 'Gemini' if (day < 21) else 'Cancer'
+        elif month == 7:
+            astro_sign = 'Cancer' if (day < 23) else 'Leo'
+        elif month == 8:
+            astro_sign = 'Leo' if (day < 23) else 'Virgo'
+        elif month == 9:
+            astro_sign = 'Virgo' if (day < 23) else 'Libra'
+        elif month == 10:
+            astro_sign = 'Libra' if (day < 23) else 'Scorpio'
+        elif month == 11:
+            astro_sign = 'Scorpio' if (day < 22) else 'Sagittarius'
 
-            hor_result =  requests.get(self.horroscope_host + astro_sign.lower())
-            hor_result_json = hor_result.json()
+        hor_result =  requests.get(self.horroscope_host + astro_sign.lower())
+        hor_result_json = hor_result.json()
+
+        if re.search(r"[Bb][Ii]*[Nn]+[Gg]+[Uu]*[Nn]+[Gg]+", args['text']):
             args['text'] = hor_result_json['horoscope']
 
             # Translate the Text
             trans_result = requests.get(self.translate_host, params={"key":"trnsl.1.1.20191221T044400Z.e189b861b5121f06.17c821d1d518276b6818ff66b3ea38beaf4b5b59", "text":args['text'], "lang": "id"})
             trans_result = marshal(trans_result, Client.translate_fields)
             args['text'] = trans_result['text']
-            print(args['text'])
 
-        if re.search(r"[Ll]+[Aa]*[Pp]+[Ee]*[Rr]+", args['text']) or re.search(r"[Mm][Aa]+[Kk][Aa]+[Nn]+", args['text']) or re.search(r"berdosa", args['text']):
+        if re.search(r"[Hh][Oo]*[Rr][Oo]*[Ss]+[Kk][Oo]*[Pp]", args['text']):
+            args['text'] = hor_result_json['horoscope']
+
+            # Translate the Text
+            trans_result = requests.get(self.translate_host, params={"key":"trnsl.1.1.20191221T044400Z.e189b861b5121f06.17c821d1d518276b6818ff66b3ea38beaf4b5b59", "text":args['text'], "lang": "id"})
+            trans_result = marshal(trans_result, Client.translate_fields)
+            args['text'] = trans_result['text']
+            return {
+                "Pesan dari TanTan": args['text']
+            }, 200
+
+        elif re.search(r"[Ll]+[Aa]*[Pp]+[AaEe]*[Rr]+", args['text']) or re.search(r"[Mm][Aa]+[Kk][Aa]+[Nn]+", args['text']):
             # Step - 1 - Check lon lat from ip
             rq = requests.get(self.geo_location + '/ipgeo', params={'ip': args['ip'], 'apiKey': self.geo_location_api_key})
             rq_json = rq.json()
